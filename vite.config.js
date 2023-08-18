0173fd04d869ddab0a25abd61cae0e8a65798fb8
Br1Ful14 <97970397+Br1Ful14@users.noreply.github.com>
2023-08-18 15:35:02 +0200

    add base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://Br1Ful14.github.io/react_portfolio_bf/'
 })
